bdb1a9522364dbe2ab1ec9813d5472a088dcf5ac
Hüseyin YİĞİT <yigit.hsyn@gmail.com>
2023-11-28 13:52:22 +0300

    naming convention updated

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-const apiu = (location.protocol == "http:") ?  "http://127.0.0.1:3000" : "https://api.signalregistry.net"
+const api_url = (location.protocol == "http:") ?  "http://127.0.0.1:3000" : "https://api.signalregistry.net"
 
-// var xhttp = new XMLHttpRequest();
+var xhttp = new XMLHttpRequest();
 // xhttp.onreadystatechange = function() {
 //     if (this.readyState == 4 && this.status == 200) {
 //       console.log(`[INFO] session_id = ${JSON.parse(xhttp.responseText).sess}`)
 //     }
 // };
 // xhttp.withCredentials = true;
-// xhttp.open("GET", apiu, true);
+// xhttp.open("GET", api_url, true);
 // xhttp.send();
 
 const parseCookie = str =>
@@ -21,11 +21,7 @@ const parseCookie = str =>
   
 
 setTimeout(()=>{
-  console.log(`[INFO] session_id    = ${parseCookie(document.cookie).sreg}`)
-  console.log(`[INFO] session_start = ${parseCookie(document.cookie).srcr}`)
-  console.log(`[INFO] session_end   = ${parseCookie(document.cookie).srex}`)
+  console.log(`[INFO] session_id    = ${parseCookie(document.cookie)._sreg_id}`)
+  console.log(`[INFO] session_start = ${parseCookie(document.cookie)._sreg_cr}`)
+  console.log(`[INFO] session_end   = ${parseCookie(document.cookie)._sreg_ex}`)
 }, 1000) 
-
-/* Abbreviations
-apiu: API Url
-*/
