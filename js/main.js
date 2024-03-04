bdbc2ec28fd69b8558556f5cda4b764d0b5b465b
Hüseyin YİĞİT <yigit.hsyn@gmail.com>
2024-03-04 16:51:14 +0300

    Session management with logging

--- js/main.js
+++ js/main.js
@@ -1,28 +1,29 @@
-const api_url = (location.protocol == "http:") ? "http://127.0.0.1:3000" : "https://api.signalregistry.net"
+log.setLevel("trace")
+// const api_url = (location.protocol == "http:") ? "http://127.0.0.1:3000" : "https://api.signalregistry.net"
 
-var request = new XMLHttpRequest();
-request.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    console.log(`[INFO] session_id    = ${parseCookie(document.cookie)._sreg_id}`)
-    console.log(`[INFO] session_start = ${parseCookie(document.cookie)._sreg_cr}`)
-    console.log(`[INFO] session_end   = ${parseCookie(document.cookie)._sreg_ex}`)
-  }
-  else if (this.readyState == 4 && this.status != 200){
-    console.log(`ERR_NO_SESSION`)
-  }
-};
-request.withCredentials = true;
-request.open("GET", api_url, true);
-request.send();
+// var request = new XMLHttpRequest();
+// request.onreadystatechange = function () {
+//   if (this.readyState == 4 && this.status == 200) {
+//     console.log(`[INFO] session_id    = ${parseCookie(document.cookie)._sreg_id}`)
+//     console.log(`[INFO] session_start = ${parseCookie(document.cookie)._sreg_cr}`)
+//     console.log(`[INFO] session_end   = ${parseCookie(document.cookie)._sreg_ex}`)
+//   }
+//   else if (this.readyState == 4 && this.status != 200){
+//     console.log(`ERR_NO_SESSION`)
+//   }
+// };
+// request.withCredentials = true;
+// request.open("GET", api_url, true);
+// request.send();
 
-const parseCookie = str =>
-  str
-    .split(';')
-    .map(v => v.split('='))
-    .reduce((acc, v) => {
-      acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
-      return acc;
-    }, {});
+// const parseCookie = str =>
+//   str
+//     .split(';')
+//     .map(v => v.split('='))
+//     .reduce((acc, v) => {
+//       acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
+//       return acc;
+//     }, {});
 
 
 
--- js/session.js
+++ js/session.js
@@ -1,10 +1,18 @@
-if(!Cookies.get("_sreg_id")) {
-  const cookie_id      = window.crypto.randomUUID()
-  const cookie_timeout = 365 * 24 * 60 * 60 * 1000 // 365 days
+const cookie_name    = "_sreg"
+const cookie_timeout = 365 * 24 * 60 * 60 * 1000 // 365 days
+const cookie_id      = Cookies.get(`${cookie_name}_id`) || window.crypto.randomUUID()
+
+if(!Cookies.get("_sreg_cr")) {
+  log.info(`[INFO] Creating new session ...`)
+
   const cookie_created = (new Date()).toISOString()
   const cookie_expire  = new Date(new Date().getTime() + cookie_timeout)
-
-  Cookies.set("_sreg_id", cookie_id, { expires: cookie_expire })
-  Cookies.set("_sreg_cr", cookie_created, { expires: cookie_expire })
-  Cookies.set("_sreg_ex", cookie_expire.toISOString(), { expires: cookie_expire })
+  
+  Cookies.set(`${cookie_name}_id`, cookie_id, { expires: cookie_expire })
+  Cookies.set(`${cookie_name}_cr`, cookie_created, { expires: cookie_expire })
+  Cookies.set(`${cookie_name}_ex`, cookie_expire.toISOString(), { expires: cookie_expire })
 }
+
+log.info(`[INFO] Session id    : ${cookie_id}`)
+log.info(`[INFO] Session start : ${Cookies.get("_sreg_cr")}`)
+log.info(`[INFO] Session expire: ${Cookies.get("_sreg_ex")}`)
