eb052852fccd77e94725970cec817685aab7b1f3
kursadsahin0 <kursatsahin565@gmail.com>
2026-03-02 12:46:39 +0300

    dist folder create

--- plamb/README.md
+++
@@ -1,7 +0,0 @@
-# Signal Registry Home Page
-
-## Environment
-- Cloudflare automatic deploy
-
-## Theme
-- May 10, 2024: Plamb v1.5
--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,10 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  build: {
+    emptyOutDir: false,
   },
 })
